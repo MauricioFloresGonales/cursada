2a802cd760f02f02726abdc3c84b1ca764376975
alumno <alumno>
2019-03-19 12:14:01 -0300

    ejer para hacer calse 2

---
+++ clase2 ejemplos/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* se ingresa numeros enteros (distintos de 0) hasta que el usuario quiera.
+ Informar:
+ a-Cantidad de numeros ingresados.
+ b-Cantidad de numeros pares.
+ c-Sunma de los positivos
+ d-Promedio de los negativos.
+ e-Maximo y minimo.
+ f-De los positivos el mas chico.
+ g-De los negativos el mas grande.
+ h-Cantidad de numeros que se encuentran entre el -10 y  el 50.
+ i-Promedio total.  */
+
+int main()
+{
+
+
+
+
+
+
+
+    return 0;
+}
